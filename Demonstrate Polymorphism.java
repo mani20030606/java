504ff1f466f63e16a8e89d328be6410439b26ddc
mani20030606 <143991447+mani20030606@users.noreply.github.com>
2023-09-13 10:15:59 +0530

    Update and rename DefaultConstructorDemo.java to Demonstrate Polymorphism.java

---
+++ Demonstrate Polymorphism.java
@@ -0,0 +1,39 @@
+//java program to demonistrate polymorphism
+//12-09-2023
+//aditya
+//PolymorphismDemo
+import java.io.*;
+class Animal
+{
+    public void AnimalSound()
+    {
+        System.out.println("an animal can make sounds");
+    }
+}
+class Cow extends Animal
+{
+    public void AnimalSound()
+    {
+        System.out.println("a cow sounds maa");
+    }
+}
+    class Cat extends Animal
+    {
+        public void AnimalSound()
+        {
+        System.out.println("a cat sound meo");
+    }
+}
+public  class PolymorphismDemo
+{
+    public static void main(String args[])
+    {
+        Animal an = new Animal();
+        Animal mycow = new Cow();
+        Animal mycat = new Cat();
+        an.AnimalSound();
+        mycow.AnimalSound();
+        mycat.AnimalSound();
+    }
+}
+
