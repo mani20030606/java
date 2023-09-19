ee1932fbd499e25d5eb8c99bf8102b3994a0ad44
mani20030606 <143991447+mani20030606@users.noreply.github.com>
2023-09-18 19:56:14 -0800

    Create UsingThiskeywordwithconstructor.java

---
+++ UsingThiskeywordwithconstructor.java
@@ -0,0 +1,15 @@
+class A {
+    A (){
+        System.out.println("inside defult constructor");
+    }
+  A (int x){
+      this();
+      System.out.println("inside parameterized constructor");
+      System.out.println("x value is"+x);
+  }
+}
+ public class ThisconstructorDemo{
+    public static void main (String args[]){
+        A a=new A(4201);
+    }
+}
