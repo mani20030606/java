7bee00b812ef76220f55f6ef8a8f040453e678f2
mani20030606 <143991447+mani20030606@users.noreply.github.com>
2023-10-15 22:25:16 -0800

    Create ArrayForEach.java

---
+++ ArrayForEach.java
@@ -0,0 +1,14 @@
+/*Java proram to dispaly the array using for loop*/
+//16/10/2023
+//ArrayforEach.java
+public class ArrayForEach{
+    public static void main (String args[]){
+        int a[]={11,22,33,44,55};
+        System.out.println("The elements of the array are");
+        for(int i : a) {
+            System.out.print(i+ " ");
+        }
+    }
+}
+        
+  
